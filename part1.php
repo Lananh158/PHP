1114f965cf6533f97c65e9f11a04509c16312423
Lananh158 <lnlananh158@gmail.com>
2024-08-21 23:48:04 +0700

    add part1

---
+++ part1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<?php
+echo "hello";
+echo '<h1> Xin chào Lan Anh </h1>'
+?>
+
+</body>
+</html>
